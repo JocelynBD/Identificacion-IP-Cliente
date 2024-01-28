4784c1ef1e8850898b03f47e1f5ac346078e1818
JocelynBD <155588614+JocelynBD@users.noreply.github.com>
2024-01-28 14:41:22 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,47 +1,33 @@
-//alert ("Hola mundo");
+//alert("Hola Mundo");
+let ip=document.getElementById("ip");
+let pais=document.getElementById("pais");
+let continente=document.getElementById("continente");
+let zona_horaria=document.getElementById("zona_horaria");
 
-let ip = document.getElementById("ip");
-let pais = document.getElementById("pais");
-let continente = document.getElementById("continente");
-let zona_horaria = document.getElementById("zona_horaria");
-
-const solicitudAPI = () => {
+const SolicitudAPI = () => {
   // Hacer una petición para un usuario con ID especifico
-axios
-
-.get("https://itp-scr-jrp-01.000webhostapp.com/php-geoip-api/index.php")
-
-  .then(function (response) {
-    // manejar respuesta exitosa
-    console.log (response.data);
-    ip.innerHTML = response.data.ip;
-    pais.innerHTML = response.data.pais;
-    continente.innerHTML = response.data.continente;
-    zona_horaria.innerHTML = response.data.zona_horaria;
-  })
-  .catch(function (error) {
-    // manejar error
-    console.log(error);
-  })
-  .finally(function () {
-    // siempre sera executado
-  });
-
-
+  axios.get("https://itp-scr-jrp-01.000webhostapp.com/php-geoip-api/index.php")
+    .then(function (response) {
+      // manejar respuesta exitosa
+      console.log(response.data);
+      ip.innerHTML = response.data.ip;
+      if (pais) pais.innerHTML = response.data.pais;
+      if (continente) continente.innerHTML = response.data.continente;
+      if (zona_horaria) zona_horaria.innerHTML = response.data.zona_horaria;
+    })
+    .catch(function (error) {
+      // manejar error
+      console.log(error);
+    })
+    .finally(function () {
+      // siempre sera executado
+    });
 };
 
-//llama al evento LOAD cada vez que se actualiza la pagina 
-//y llama a la version solicitudAPI que tiene la rutina
-//de llamar a API desde Axios
-window.addEventListener('load',solicitudAPI );
-
-
-
-
-
-
-
-
+// Llama al evento LOAD cada vez que se refresca o se actualiza la pagina 
+// Y llama a la función solicitud API que tiene la rutina de llama a la API
+// desde el Axios 
+window.addEventListener('load', SolicitudAPI);
 
 
 
